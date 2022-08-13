39a2d6a365bc8d89165ac1b8d3226c94ba823f31
Radion Merzkyakov <ya.irodya54@yandex.ru>
2022-08-13 11:36:05 +0300

    add all

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,39 +2,39 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.dao.CarDaoImpl;
 import web.model.Cars;
-import web.service.CarServiceImpl;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private CarServiceImpl carService = new CarServiceImpl();
-    @GetMapping("/")
-    public String getCarsView(Model model) {
 
-        return "cars";
+    private final CarDaoImpl dao;
+
+    public CarController(CarDaoImpl dao) {
+        this.dao = dao;
     }
 
-    @GetMapping("/showCars")
-    public String getShowCars(@RequestParam("count") int count, Model model) {
-        return "showCars";
+    @GetMapping("showCars")
+    public String showAllCars(@RequestParam(defaultValue = "5") int count, Model model) {
+        model.addAttribute("allCars", dao.getCarsListByCount(count));
+
+        return "cars/showCars";
     }
 
-    @GetMapping("/enterCars")
-    public String getEnterCars(@ModelAttribute("cars") Cars car, Model model) {
-        model.addAttribute("brandCar", car.getBrand());
-        model.addAttribute("modelCar", car.getModel());
-        model.addAttribute("gosNumberCar", car.getGosNumber());
-//        Cars cars = new Cars(model.getAttribute("brandCar").toString(),
-//                model.getAttribute("modelCar").toString(),
-//                model.getAttribute("gosNumberCar").toString());
-//        carService.addCar(cars);
-//        System.out.println(cars);
-        System.out.println(model.getAttribute("brandCar"));
-        return "enterCars";
+    @GetMapping("/enterCar")
+    public String addCar(Model model) {
+        model.addAttribute("car", new Cars());
+        return "cars/enterCar";
     }
+
+    @PostMapping()
+    public String postMethodCar(@ModelAttribute("car") Cars car) {
+        dao.addCar(car);
+        return "redirect:/cars/showCars";
+    }
+
+
 }
---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,40 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Cars;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CarDaoImpl implements CarDao{
+    private List<Cars> carsList;
+
+    public CarDaoImpl() {
+        carsList = new ArrayList<>();
+        carsList.add(new Cars("Audi", "A7", "A000AA178"));
+        carsList.add(new Cars("BMW", "3", "A001AA178"));
+        carsList.add(new Cars("Lada", "Vesta", "A003AA178"));
+    }
+
+
+    @Override
+    public void addCar(Cars car) {
+
+        carsList.add(car);
+    }
+
+    public List<Cars> getCarsListByCount(int count) {
+        if (count==0 || count> carsList.size()) {
+            return carsList;
+        } else {
+            List<Cars> newList = new ArrayList<>();
+            newList = carsList.stream()
+                    .limit(count)
+                    .toList();
+            return newList;
+        }
+    }
+    public  List<Cars> getAllCars () {
+        return carsList;
+    }
+}
